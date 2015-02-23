3c54af26d12cfad4d29c4337f3333942800253d7
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-02-23 17:25:29 +0100

    Add a filtered file

--- spontal_filter.py
+++ spontal_filter.py
@@ -5,9 +5,20 @@
 # Info: 
 
 import sys
-
+import xml.etree.ElementTree as ET
 
 def main():
+    tree = ET.parse('spontal.xml')
+    root = tree.getroot()
+
+    print(root.tag)
+
+    for child in root:
+        for child in child:
+            print(child.tag, child.text)
+
+
+    tree.write('filtered_spontal.xml')
 
 
 if __name__ == "__main__":
