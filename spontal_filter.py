96b1f9178b0bf16240f60e4fea92ab9fed4487b2
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-02-24 15:44:48 +0100

    Added If condition and removal of incorrect data

--- spontal_filter.py
+++ spontal_filter.py
@@ -11,11 +11,17 @@ def main():
     tree = ET.parse('spontal.xml')
     root = tree.getroot()
 
-    print(root.tag)
-
     for child in root:
-        for child in child:
-            print(child.tag, child.text)
+        BOTTOM_HZ = (child.find('BOTTOM_HZ').text)
+        TOP_HZ = (child.find('TOP_HZ').text)
+        F0_START = (child.find('F0_START').text)
+        F0_END = (child.find('F0_END').text)
+
+        if BOTTOM_HZ <= F0_START <= TOP_HZ and BOTTOM_HZ <= F0_END <= TOP_HZ:
+            continue
+        else:
+            print("removed")
+            root.remove(child)
 
 
     tree.write('filtered_spontal.xml')
